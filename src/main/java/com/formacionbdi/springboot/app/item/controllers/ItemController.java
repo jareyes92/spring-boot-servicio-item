7977a5b05db3f51f68909410453cebff5033abdb
Jose Antonio Reyes <jose.reyes@innoway.es>
2023-03-07 12:32:18 +0100

    Spring Cloud Config Server para centralizar las configuraciones en un repositorio git

--- src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -8,10 +8,16 @@ import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
@@ -20,10 +26,16 @@ public class ItemController {
     private final Logger logger = LoggerFactory.getLogger(ItemController.class);
 
     @Autowired
+    private Environment env;
+
+    @Autowired
     private CircuitBreakerFactory cbFactory;
 
     @Autowired
     private ItemService itemService;
+
+    @Value("${configuracion.texto}")
+    private String texto;
 
     @GetMapping("/listar")
     public List<Item> listar(@RequestParam(name = "nombre", required = false) String nombre, @RequestHeader(name = "token-request", required = false) String token){
@@ -74,5 +86,20 @@ public class ItemController {
         producto.setPrecio(500.00);
         item.setProducto(producto);
         return CompletableFuture.supplyAsync(()-> item) ;
+    }
+
+    @GetMapping("/obtener-config")
+    public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+        logger.info(texto);
+        logger.info(puerto);
+        Map<String, String> json = new HashMap<>();
+        json.put("texto", texto);
+        json.put("puerto", puerto);
+
+        if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")){
+            json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+            json.put("autor.email", env.getProperty("configuracion.autor.email"));
+        }
+        return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
     }
 }
